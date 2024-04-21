c5b03d3a7a0c4662012571a386ba130115cada96
JDIVE <26393518+JDIVE@users.noreply.github.com>
2024-04-21 20:18:19 +0100

    Added tailscale aliases

--- sh.nix
+++ sh.nix
@@ -2,7 +2,14 @@
 let
   myAliases = {
     ll = "ls -l";
+    
     ".." = "cd ..";
+
+    ts = "tailscale";
+    tssh = "tailscale ssh";
+    tst = "tailscale status";
+    tsu = "tailscale up --ssh --operator=$USER";
+    tsd = "tailscale down";
   };
   in
 {
